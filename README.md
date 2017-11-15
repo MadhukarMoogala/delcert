920b69d362510700e46ff5e53e278759e9d32364
Madhukar Moogala <madhukar.moogala@autodesk.com>
2017-11-15 12:47:58 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,125 @@
-# delcert
-Small Win32 Console app to remove signature files which are unsignable for obvious reasons
+[![Scrutinizer Build](https://img.shields.io/scrutinizer/build/g/filp/whoops.svg)]() [![Scrutinizer Build](https://img.shields.io/badge/Visual%20Studio-2015-brightgreen.svg)]()
+##### This is a small win32 utility to remove the signature of file which are unsignable for obvious reasons.
+
+
+```
+Stripping file: C:\OEM\OEMOpera\OperaWM.exe.
+Succeeded.
+
+C:\>delcert.exe OEM\OEM_Lang_0409\*.mui
+
+Target file(s): OEM_Lang_0409\*.mui
+
+Stripping file: C:\[snip]\OEM\OEM_Lang_0409\aboutprop.dll.0409.mui.
+Succeeded.
+
+Stripping file: C:\[snip]\OEM\OEM_Lang_0409\BKLBrightness.dll.0409.mui.
+Succeeded.
+```
+
+Steps To Build
+-------
+ * Open the delCert.vcxproj in VS2015
+ * Include library 'Imagehlp.lib'
+ * Include library folder 'C:\Program Files (x86)\Windows Kits\8.1\Lib\winv6.3\um\x86'
+ * Msbuild /t:build /p:Configuration=debug;Platform=win32
+
+Authors
+-----
+Deepred source - https://forum.xda-developers.com/showthread.php?p=2508061#post2508061
+
+
+
+Format of a Signed File
+-----------------------
+
+A signed file is a standard Windows PE executable with signature data appended
+to it.  A PE file begins with a 16-bit MSDOS executable stub (which just prints
+a message that you can't run the program and exits).  Following this is the
+real data, organised in a COFF-like format.  The general COFF header which is
+always present is followed by another header which contains information on
+executables (it's referred to as an optional header, but it's always present
+for executables).  This header defines a number of data directories which
+contain data sections within the executable.  One of these directory entries is
+an (undocumented) IMAGE_DIRECTORY_ENTRY_SECURITY, which is used to store the
+signature: Hash the entire file, add a security directory entry large enough to
+contain the signature, and write the signature at that position in the file.
+
+The signing process is as follows:
+
+1. Hash the file, skipping the file checksum (another undocumented attribute,
+   but there are standard Windows functions to calculate it) and the
+   IMAGE_DIRECTORY_ENTRY_SECURITY entry.
+2. Generate the signature and append it to the file.
+3. Write the signature position and size (padded out to a multiple of 16 bytes)
+   into the IMAGE_DIRECTORY_ENTRY_SECURITY.  The signature position is the end
+   of the file, since it's simply appended to the existing data.
+4. Update the file checksum.
+
+The security information begins with a 32-bit length (which is ignored) and a
+32-bit flags field which should be set to 0x00020200.  Most of the flags are
+unused, although changing the first 0x02 to a 1 gives an invalid signature, and
+changing the second one to anything other than 0x02 results in the signature
+check silently failing with no error message or other indication.
+
+Immediately following this is a complex PKCS #7 signature record which contains
+a detached signature for the rest of the file.  The outer wrapper is PKCS #7
+signedData, with the signed content being an spcIndirectDataContext (an MS-
+defined data type) which contains an (obsolete) spcPelmageData record and an
+MD5 hash of the file.  The exact format is:
+
+    SEQUENCE {
+      SEQUENCE {
+        OBJECT IDENTIFIER spcPelmageData (1 3 6 1 4 1 311 2 1 15)
+        SEQUENCE {
+          BIT STRING (length 0)
+          [0] {
+            [2] {
+              [0] '.<.<.<.O.b.s.o.l.e.t.e.>.>.>'
+              }
+            }
+          }
+        }
+      SEQUENCE {
+        SEQUENCE {
+          OBJECT IDENTIFIER md5 (1 2 840 113549 2 5)
+          NULL
+          }
+        OCTET STRING
+          xx xx xx xx xx xx xx xx xx xx xx xx xx xx xx xx
+        }
+      }
+    }
+  }
+
+where the x's are the MD5 hash of the file and the content-type for the whole
+is spcIndirectDataContext (1 3 6 1 4 1 311 2 1 4).  You can use a toolkit such
+as cryptlib to generate this type of data, as well as creating the special
+AuthentiCode certs you need for the signing.
+
+The signed content is followed by the standard PKCS #7 signedData fields, first
+a certificate chain containing a complete chain of certs from a known CA to an
+end-users AuthentiCode signing certificate and then information on the signer
+(that is, data to identify the AuthentiCode certificate which signed the whole
+thing), an identifier for the hash algorithm used (again, MD5), and some
+authenticated attributes.  These contain more MS-defined attributes, typically
+spcSpOpusInfo and spcStatementType which are used to provide information on the
+signer (eg whether they're using an individual or commercial signing
+certificate).  Also included is a messageDigest attribute which contains the
+MD5 hash of the spcIndirectDataContext content of the signedData.  Finally, the
+authenticated attributes are hashed and signed, with the signature being
+included at the end of the collection of signature information (this is the
+standard PKCS #7 signature process).  Again, this is standard stuff which you
+can create using cryptlib.
+
+To create the signature, you need to hash the entire file (skipping the
+checksum and IMAGE_DIRECTORY_ENTRY_SECURITY as explained above) and write the
+hash into the spcIndirectDataContext content record.  Then hash this and write
+the hash and other attributes into the authenticated attributes.  Finally, hash
+the authenticated attributes and append the signature to the end of the
+signature record.  This means that each collection of data is covered by its
+own hash, which is then passed up the chain to the next level until it reaches
+the signature.  If you're using an existing crypto toolkit to do this, create a
+PKCS #7 signedData item, set the content-type to spcIndirectDataContext, drop
+in the spcIndirectDataContext content itself, and sign it, the rest will be
+handled by the PKCS #7 signing code.
---
+++ delCert/delCert.cpp
@@ -0,0 +1,244 @@
+
+// delcert.cpp : Defines the entry point for the console application.
+//
+// An app to make hiterto unsignable file(s) signable again.
+//
+// 8/10/2006 - Drew
+// 7/19/2008 - deepred
+
+//This is orginal work belongs to author 'deepred' https://forum.xda-developers.com/showthread.php?p=2508061#post2508061
+
+//moogalm: Just migrated the code to VS2015, added function for error message.
+
+
+#include "stdafx.h"
+
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <stdio.h>
+#include <imagehlp.h>
+#include <malloc.h>
+#include <tchar.h>
+
+#define REGULARFILE(x) ((x) & ~(FILE_ATTRIBUTE_DIRECTORY | FILE_ATTRIBUTE_OFFLINE | FILE_ATTRIBUTE_ENCRYPTED | FILE_ATTRIBUTE_VIRTUAL))
+
+DWORD StripAuthenticode(LPTSTR);
+
+int wmain(DWORD argc, LPTSTR argv[]) {
+	WIN32_FIND_DATA FindFileData = { 0 };
+	HANDLE hFindFile = INVALID_HANDLE_VALUE;
+	DWORD dwResult = ERROR_SUCCESS;
+	TCHAR lpBuffer[4096] = { 0 };
+	LPTSTR lpPart = NULL;
+	TCHAR pszFileDirectory[4096] = { 0 };
+	TCHAR pszFileFullName[8192] = { 0 };
+
+	_tprintf(_TEXT("\n"));
+	if (2 != argc || 0 == _tcscmp(_TEXT("-?"), argv[1]) || 0 == _tcscmp(_TEXT("/?"), argv[1])) {
+		_tprintf(_TEXT("%s takes one parameter - a file name to strip of its embedded Authenticode signature.\n\n"), argv[0]);
+		return 0;
+	}
+
+	if (0 == GetFullPathName(argv[1], 4096, lpBuffer, &lpPart)) {
+		_tprintf(_TEXT("Failed to GetFullPathName."));
+		return 0;
+	}
+	if (_tcsstr(lpBuffer, lpPart)) {
+		_tcsncpy_s(pszFileDirectory, lpBuffer, _tcslen(lpBuffer) - _tcslen(lpPart));
+	}
+
+	_tprintf(_TEXT("Target file(s): %s\n\n"), argv[1]);
+	hFindFile = FindFirstFile(argv[1], &FindFileData);
+	if (INVALID_HANDLE_VALUE == hFindFile) {
+		dwResult = GetLastError();
+		_tprintf(_TEXT("Invalid File Handle. GLE = 0x%08x\n"), dwResult);
+	}
+	else {
+		if (REGULARFILE(FindFileData.dwFileAttributes)) {
+			if (-1 == _stprintf_s(pszFileFullName, 8192, _TEXT("%s%s"), pszFileDirectory, FindFileData.cFileName)) {
+				dwResult = GetLastError();
+				_tprintf(_TEXT("Failed to copy pszFileName to string of chars. GLE == x0%08x\n"), dwResult);
+				goto exit;
+			}
+			dwResult = StripAuthenticode(pszFileFullName);
+		}
+		while (TRUE) {
+			if (FindNextFile(hFindFile, &FindFileData)) {
+				if (REGULARFILE(FindFileData.dwFileAttributes)) {
+					if (-1 == _stprintf_s(pszFileFullName, 8192, _TEXT("%s%s"), pszFileDirectory, FindFileData.cFileName)) {
+						dwResult = GetLastError();
+						_tprintf(_TEXT("Failed to copy pszFileName to string of chars. GLE == x0%08x\n"), dwResult);
+						goto exit;
+					}
+					dwResult = StripAuthenticode(pszFileFullName);
+				}
+			}
+			else {
+				dwResult = GetLastError();
+				break;
+			}
+		}
+	}
+
+exit:
+	if (INVALID_HANDLE_VALUE != hFindFile) FindClose(hFindFile);
+	return dwResult;
+}
+
+//Caller should deallocate the memory after use.
+TCHAR* PrintErrorMSG(DWORD dwErr)
+{
+
+	WCHAR   wszMsgBuff[512];  // Buffer for text.
+
+	DWORD   dwChars;  // Number of chars returned.
+
+					  // Try to get the message from the system errors.
+	dwChars = FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM |
+		FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL,
+		dwErr,
+		0,
+		wszMsgBuff,
+		512,
+		NULL);
+
+	if (0 == dwChars)
+	{
+		// The error code did not exist in the system errors.
+		// Try Ntdsbmsg.dll for the error code.
+
+		HINSTANCE hInst;
+
+		// Load the library.
+		hInst = LoadLibrary(L"Ntdsbmsg.dll");
+		if (NULL == hInst)
+		{
+			return _T("cannot load Ntdsbmsg.dll\n");  // Could 'return' instead of 'exit'.
+		}
+
+		// Try getting message text from ntdsbmsg.
+		dwChars = FormatMessage(FORMAT_MESSAGE_FROM_HMODULE |
+			FORMAT_MESSAGE_IGNORE_INSERTS,
+			hInst,
+			dwErr,
+			0,
+			wszMsgBuff,
+			512,
+			NULL);
+
+		// Free the library.
+		FreeLibrary(hInst);
+
+	}
+
+	// Display the error message, or generic text if not found.
+	TCHAR* txtMsg = new TCHAR[512];
+	swprintf(txtMsg, 512, _TEXT("Error value: %d Message: %ws\n"), dwErr, dwChars ? wszMsgBuff : L"Error message not found.");
+	return txtMsg;
+
+}
+DWORD StripAuthenticode(LPTSTR pszFileName) {
+	HANDLE hFile = INVALID_HANDLE_VALUE;
+	LOADED_IMAGE image;
+	DWORD dwResult = ERROR_SUCCESS;
+	LPSTR lpszImageName = NULL;
+	size_t cchImageName = 0;
+
+	_tprintf(_TEXT("Stripping file: %s.\n"), pszFileName);
+	hFile = CreateFile(pszFileName, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_DELETE, NULL, OPEN_EXISTING, 0, NULL);
+	if (INVALID_HANDLE_VALUE == hFile) {
+		dwResult = GetLastError();
+		TCHAR* errorMSg = PrintErrorMSG(dwResult);
+		_tprintf(errorMSg);
+		delete[] errorMSg;
+		goto cleanupAndExit;
+	}
+
+	if (ImageRemoveCertificate(hFile, 0)) {
+		goto cleanupAndExit;
+	}
+	else {
+		dwResult = GetLastError();
+		TCHAR* errorMSg = PrintErrorMSG(dwResult);
+		_tprintf(errorMSg);
+		delete[] errorMSg;
+		_tprintf(_TEXT("ImageRemoveCertificate failed with error 0x%08x\n"), dwResult);
+		if (ERROR_INVALID_PARAMETER != dwResult) {
+			goto cleanupAndExit;
+		}
+		else {
+			_tprintf(_TEXT("This happens when there's a listing in IMAGE_DIRECTORY_SECURITY\nin the PE's header, but the actual Authenticode signature has been stripped.\nLet's fix that ...\n"));
+			dwResult = ERROR_SUCCESS;
+		}
+	}
+
+	if (CloseHandle(hFile)) hFile = INVALID_HANDLE_VALUE;
+	// This is somewhat sloppy, but if we're here we've almost certainly found a PE with an
+	// IMAGE_DIRECTORY_SECURITY that has nonzero SizeOfRawData and/or PointerToRawData,
+	// but the actual signature (that raw data) has been removed.
+	//
+	// What causes this? IIRC, strong name signing something that's already been Authenticode-signed.
+	//
+	// The workaround is to crack open the PE and write zeros into the directory entry so that everything
+	// that eventually calls through the Image*Certificate* APIs won't choke.
+
+	cchImageName = _tcslen(pszFileName) + 1;
+	lpszImageName = (LPSTR)malloc(cchImageName); // Yeah - so I'm all old-school mallocy!
+	if (!lpszImageName) {
+		dwResult = GetLastError();
+		TCHAR* errorMSg = PrintErrorMSG(dwResult);
+		_tprintf(errorMSg);
+		delete[] errorMSg;
+		_tprintf(_TEXT("Malloc failed. GLE == 0x%08x\n"), dwResult);
+		goto cleanupAndExit;
+	}
+
+	if (-1 == sprintf_s(lpszImageName, cchImageName, "%S", pszFileName)) {
+		dwResult = GetLastError();
+		TCHAR* errorMSg = PrintErrorMSG(dwResult);
+		_tprintf(errorMSg);
+		delete[] errorMSg;
+		_tprintf(_TEXT("Failed to copy pszFileName to string of chars. GLE == x0%08x\n"), dwResult);
+		goto cleanupAndExit;
+	}
+
+	if (!MapAndLoad(lpszImageName, NULL, &image, FALSE, FALSE)) {
+		dwResult = GetLastError();
+		PrintErrorMSG(dwResult);
+		_tprintf(_TEXT("MapAndLoad failed. GLE == 0x%08x"), dwResult);
+		goto cleanupAndExit;
+	}
+
+	_tprintf(_TEXT("certificates->Size == 0x%08x\n"), image.FileHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_SECURITY].Size);
+	_tprintf(_TEXT("certificates->VA == 0x%08x\n"), image.FileHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_SECURITY].VirtualAddress);
+
+	if (image.FileHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_SECURITY].Size ||
+		image.FileHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_SECURITY].VirtualAddress) {
+		_tprintf(_TEXT("Setting both fields to zero ...\n"));
+		image.FileHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_SECURITY].Size = 0;
+		image.FileHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_SECURITY].VirtualAddress = 0;
+	}
+	else {
+		_tprintf(_TEXT("Fields are set to zero already. Skipping ...\n"));
+	}
+
+	if (!UnMapAndLoad(&image)) {
+		dwResult = GetLastError();
+		TCHAR* errorMSg = PrintErrorMSG(dwResult);
+		_tprintf(errorMSg);
+		delete[] errorMSg;
+		_tprintf(_TEXT("Failed to UnMapAndLoad. GLE == 0x%08x\n"), dwResult);
+		goto cleanupAndExit;
+	}
+
+cleanupAndExit:
+
+	if (INVALID_HANDLE_VALUE != hFile) CloseHandle(hFile);
+	if (lpszImageName) free(lpszImageName);
+	if (ERROR_SUCCESS == dwResult) _tprintf(_TEXT("Succeeded.\n"));
+	_tprintf(_TEXT("\n"));
+	return dwResult;
+}
+
